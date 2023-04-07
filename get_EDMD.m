1a3e15355cbd5dd241adf3d196a1abc56f81103a
Sriram Krishnamoorthy <103785089+sriram-2502@users.noreply.github.com>
2023-04-07 16:31:12 -0400

    bug fixes

--- get_EDMD.m
+++ get_EDMD.m
@@ -6,22 +6,24 @@ params = get_params();
 % states X = [x dx R wb]'
 x0 = [0;0;0]; dx0 = [0;0;0];
 R0 = eye(3); wb0 = [0;0;0];
-X0 = [x0,dx0,R0,wb0];
+X0 = [x0;dx0;R0(:);wb0];
 
 % generate n random inputs for 4 thrusters
-% todo scale inputs to match geometric control paper
-n = 100;
+n = 10;
 mu = [0;0;0;0];
-Sigma = diag([1;1;1;1]);
+Sigma = diag([100;100;100;100]);
 U_rnd = mvnrnd(mu,Sigma,n);
 
 % simulate random inputs for 50s to get trajectories
+% initial condition is from rest (from ground)
 X_rnd = [];
 t_span = 0:0.1:50;
 for i=1:n
-    U = U_rnd(i,:);
-    [t,X] = ode45(@(t,X)dynamics_SRB(t,X0,U,params),t_span,X0);
-    X_rnd = [X_rnd, X];
+    U = U_rnd(i,:)';
+    U(1) = params.mass*params.g/4 + U(1);
+    [t,X] = ode45(@(t,X)dynamics_SRB(t,X,U,params),t_span,X0);
+    %plot3(X(:,1),X(:,2),X(:,3)); hold on
+    X_rnd = [X_rnd;X'];
 end
 
 EDMD = X_rnd;
