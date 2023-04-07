17772eb41af0dab81a947b9855523af747148a94
Sriram Krishnamoorthy <103785089+sriram-2502@users.noreply.github.com>
2023-04-07 16:31:41 -0400

    added gravity as parameter

--- get_params.m
+++ get_params.m
@@ -20,8 +20,11 @@ m = 4.34; % kg
 J = diag([0.0820, 0.0845, 0.1377]);
 db = 0.315; % distance between the com and rotors
 c_torque = 8.004e-4; % torque constrant
+g = 9.81; % gravity
 
 params.mass = m;
 params.J = J;
 params.d = db;
 params.c_troque = c_torque;
+params.g = g;
+
