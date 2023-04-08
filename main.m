99e357de2b50c492978378150c0f983702cd8c6e
sriramk <sriramk@clemson.edu>
2023-04-07 20:25:32 -0400

    todo- normalize A B matrices to reduce error in least squares

--- main.m
+++ main.m
@@ -2,19 +2,22 @@ clc; clear; close all;
 
 %% get robot parameters
 params = get_params();
-
+dt = 1e-2;
 %% generate random data
+% generate random data starting from same initial condition
+% each traj is generated using contant control input
+% generate random control inputs to get different trajectories
 x0 = [0;0;0]; dx0 = [0;0;0];
 R0 = eye(3); wb0 = [0;0;0];
 X0 = [x0;dx0;R0(:);wb0];
 
 n_control = 1000; % number of random controls to apply
-t_traj = 0:0.01:1; % traj length to simulate (s)
-show_plot = false;
+t_traj = 0:dt:1; % traj length to simulate (s)
+show_plot = true;
 [X, U] = get_trajectories(X0,n_control,t_traj,show_plot);
 
 %% get EDMD matrices
-n_basis = 1;
+n_basis = 10;
 EDMD = get_EDMD(X, U, n_basis);
 A = EDMD.A;
 B = EDMD.B;
@@ -26,12 +29,12 @@ U1 = U(:,1:end-1);
 Z2 = Z(:,2:end);
 Z2_predicted = A*Z1 + B*U1;
 Z2_error = Z2 - Z2_predicted;
-Z2_mse = sqrt(mean(Z2_error(:).^2));
+Z2_mse = sqrt(mean(Z2_error(:).^2))
 
 %% check prediction on new control input
 
 n_control = 1; % number of random controls to apply
-t_traj = 0:0.01:1; % traj length to simulate (s)
+t_traj = 0:1e-3:0.1; % traj length to simulate (s)
 [X, U] = get_trajectories(X0,n_control,t_traj,show_plot);
 u = U(:,1); % get first input since input in constant along each traj
 
@@ -40,8 +43,8 @@ basis = get_basis(X(:,1),n_basis);
 z0 = [X(1:3,1); X(4:6,1); basis];
 
 % propagate z0 for prediction
-n_prediction = 10;
-z = z0; Z_pred = [z0];
+n_prediction = 100; % 10 timesteps
+z = z0; Z_pred = [z0]; % add initial
 for i = 1:n_prediction
     z_next = A*z + B*u;
     z = z_next;
@@ -50,7 +53,7 @@ end
 
 % get Z2 for comparion
 Z_true = [];
-for i = 1:n_prediction+1
+for i = 1:n_prediction+1 % compare n+1 timesteps
     x = X(:,i);
     basis = get_basis(x,n_basis);
     z = [x(1:3); x(4:6); basis];
@@ -58,11 +61,11 @@ for i = 1:n_prediction+1
 end
 
 Z2_error = Z_true - Z_pred;
-Z2_mse = sqrt(mean(Z2_error(:).^2));
+Z2_mse_prediction = sqrt(mean(Z2_error(:).^2))
 
 figure(2)
 plot3(Z_true(1,:), Z_true(2,:), Z_true(3,:)); hold on
-plot3(Z2_error(1,:), Z2_error(2,:), Z2_error(3,:), '--'); hold on
+plot3(Z_pred(1,:), Z_pred(2,:), Z_pred(3,:), '--'); hold on
 legend('true','predicted')
 grid on;
 axis square
